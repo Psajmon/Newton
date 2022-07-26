6281571a182ac64c7a366f8c8fddfb70b5ec8e15
Psajmon <pawel.machelski@gmail.com>
2022-07-26 16:42:03 +0200

    Initial commit

---
+++ Assets/Scripts/DrawLine.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawLine : MonoBehaviour
+{
+    public LineRenderer lineRenderer;
+    public Transform drawTo;
+    private void Start()
+    {
+        if (lineRenderer == null) Debug.LogError("lineRenderer not assigned in editor");
+        if (drawTo == null) Debug.LogError("drawTo not assigned in editor");
+
+        lineRenderer.SetPosition(0, transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lineRenderer.SetPosition(1, drawTo.transform.position);
+    }
+}
